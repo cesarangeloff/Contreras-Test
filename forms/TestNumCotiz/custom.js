7d47c4985881ea8526f7acfcad459fce775fb0fb
cesar <cesar.angeloff@nortedesarrollos.com>
2024-01-19 12:43:49 -0300

    ..

--- forms/TestNumCotiz/custom.js
+++ forms/TestNumCotiz/custom.js
@@ -23,23 +23,50 @@ const vm = new Vue({
   vuetify: new Vuetify(),
   data() {
     return {
+      dialogHistorial: false,
       viewMode: true,
+      viewTrigger: false,
       procesoFinalizado: false,
       WKNumState: 0,
       WKDef: "",
       model: {
+      itemsHistorial: [{}],//getDataset(), //[{}],
         numcotiz: getCotiz(),
         // numcotiz: '',
         fecha: fechaFormulario.innerHTML == '' ? fechaDelDia() : fechaFormulario.innerHTML,
         fechaSeg: fechaFormulario.innerHTML == '' ? fechaDelDia(7) : fechaFormulario.innerHTML,
       },
       clientes: getClientes(),
+      sellers: getSellers(),
+      monedas: getMonedas(),
+      paidmetods: getPaidMethod(),
       // clientes: [],
       required: [(v) => !!v || "Campo requerido"],
     };
   },
   computed: {
     console: () => console,
+    headersHistorial() {
+      return [
+              { text: 'Nro. Cotizacion', align: 'center', value: 'nroCotiz', width: '10rem', sortable: false },
+              { text: 'Cliente', align: 'center', value: 'cliente', width: '10rem', inputType: 'text', sortable: false },
+              { text: '', align: 'center', value: 'deleteRow', type: 'icon', width: '2rem', sortable: false},
+      ];
+    },
+
+    clientesMod() {
+      return this.clientes.map(cliente => ({
+        ...cliente,
+        claveClientes: `${cliente.cod} - ${cliente.name}`
+      }));
+    },
+
+    sellersMod() {
+      return this.sellers.map(seller => ({
+        ...seller,
+        claveSellers: `${seller.cod} - ${seller.name}`
+      }));
+    },
   },
   methods: {
     init() {
@@ -105,16 +132,39 @@ const vm = new Vue({
 
       return chunks;
     },
+
+    getClientSelect(){
+      const clienteSel = this.clientes.find(cliente => cliente.cod === this.model.codCli);
+      if(clienteSel){
+        this.model.razSoc = clienteSel.name;
+        this.model.CUITCli = clienteSel.cuit;
+        this.viewTrigger = true;
+      }else{
+        this.model.razSoc = '';
+        this.model.CUITCli = '';
+        this.viewTrigger = false;
+      }
+    },
   },
 });
+
+function getHistorial() {
+  var constraints = new Array();
+  //constraints.push(DatasetFactory.createConstraint("sqlLimit", "10", "10", ConstraintType.MUST));
+  //var dataset = DatasetFactory.getDataset("dsSolicitudCotizacion", ['numero_cotizacion'], constraints, ['numero_cotizacion DESC']);
+  
+  var dataset = DatasetFactory.getDataset("dsSolicitudCotizacion", null, constraints, null);
+
+  return dataset;
+};
+
 
 function getCotiz() {
   var valorActual = 0;
   var valorNuevo = 0;
   var constraints = new Array();
   constraints.push(DatasetFactory.createConstraint("sqlLimit", "10", "10", ConstraintType.MUST));
-  var dataset = DatasetFactory.getDataset("dsSolicitudCotizacion", ['numero_cotizacion'], constraints, ['numero_cotizacion DESC']);
-  var dataset2 = DatasetFactory.getDataset("dsSolicitudCotizacion", null, constraints, null);
+  var dataset = DatasetFactory.getDataset("dsTestNumCotiz", ['numero_cotizacion'], constraints, ['numero_cotizacion DESC']);
   
   if (dataset.values.length > 0) {
     valorActual= parseInt(dataset.values[0].numero_cotizacion);
@@ -128,20 +178,69 @@ function getCotiz() {
 
 function getClientes(idCliente) {
   var constraints = []
-  var jsonClientes = ''
   var clientesResult = []
   if (idCliente){
     constraints.push(DatasetFactory.createConstraint('searchKey', idCliente, idCliente, ConstraintType.MUST));
   }
-  constraints.push(DatasetFactory.createConstraint('pageSize', "100", "100", ConstraintType.MUST));
+  constraints.push(DatasetFactory.createConstraint('pageSize', "100000", "100000", ConstraintType.MUST));
 
   var clientes = DatasetFactory.getDataset("clientes_Protheus", null, constraints, null);
       for (var j = 0; j < clientes.values.length; j++) {
-    jsonClientes = ''
-          clientesResult.push({ name: clientes.values[j]['name'], cod: clientes.values[j]['id'], })
+          clientesResult.push({ name: clientes.values[j]['name'], cod: clientes.values[j]['id'],cuit: clientes.values[j]['cuit'] })
   }
 
   return clientesResult
+};
+
+
+function getSellers(idSeller) {
+  var constraints = []
+  var sellersResult = []
+  if (idSeller){
+    constraints.push(DatasetFactory.createConstraint('searchKey', idSeller, idSeller, ConstraintType.MUST));
+  }
+  constraints.push(DatasetFactory.createConstraint('pageSize', "100", "100", ConstraintType.MUST));
+
+  var sellers = DatasetFactory.getDataset("vendedores_Protheus", null, constraints, null);
+      for (var j = 0; j < sellers.values.length; j++) {
+        sellersResult.push({ name: sellers.values[j]['name'], cod: sellers.values[j]['cod'] })
+  }
+
+  return sellersResult
+};
+
+
+function getMonedas(idMoneda) {
+  var constraints = []
+  var monedasResult = []
+  if (idMoneda){
+    constraints.push(DatasetFactory.createConstraint('searchKey', idMoneda, idMoneda, ConstraintType.MUST));
+  }
+  constraints.push(DatasetFactory.createConstraint('pageSize', "100", "100", ConstraintType.MUST));
+
+  var monedas = DatasetFactory.getDataset("monedas_Protheus", null, constraints, null);
+      for (var j = 0; j < monedas.values.length; j++) {
+        monedasResult.push({ cod: monedas.values[j]['cod'], desc: monedas.values[j]['description'],symb: monedas.values[j]['symb'], desc2:monedas.values[j]['symb'] + ' - ' + monedas.values[j]['description']})
+  }
+
+  return monedasResult
+};
+
+
+function getPaidMethod(idMethod) {
+  var constraints = []
+  var methodsResult = []
+  if (idMethod){
+    constraints.push(DatasetFactory.createConstraint('searchKey', idMethod, idMethod, ConstraintType.MUST));
+  }
+  constraints.push(DatasetFactory.createConstraint('pageSize', "100", "100", ConstraintType.MUST));
+
+  var paidmethods = DatasetFactory.getDataset("metodosDePago_Protheus", null, constraints, null);
+      for (var j = 0; j < paidmethods.values.length; j++) {
+        methodsResult.push({ cod: paidmethods.values[j]['cod'], desc: paidmethods.values[j]['description'],cond: paidmethods.values[j]['condition'] })
+  }
+
+  return methodsResult
 };
 
 
@@ -174,5 +273,4 @@ function formatoFecha(fecha){
   return dia+"/"+mes+"/"+ano;
   
 }
-
 
